91a9d65bb9676316d92e24200d9d6ef5d8f14e2f
Arkan Syafiq At'taqy <91069910+arknsa@users.noreply.github.com>
2024-06-23 05:41:09 +0700

    Delete code/js directory

--- code/js/register.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.register-btn');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert(`Anda memilih ${this.textContent}`);
-        });
-    });
-});
